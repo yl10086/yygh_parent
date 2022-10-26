1369992491f6953212a9645398c4f414ff741e44
a1978512164 <1978512164@qq.com>
2022-10-26 17:44:31 +0800

    1、 搭建医院模块service-hosp

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author YeLei
+ * @Date 2022/10/26 17:35
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+
+}
